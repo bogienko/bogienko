2575f931039c3ef6e2c0dc8f843e262eea67e443
bogienko <bogienko@mail.ru>
2017-01-10 17:55:24 +0200

    Kalkuliator

--- Program.java
+++ Program.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class Program
 {
 	public static void main (String [] args)
 	{
 	System.out.println ("Hello world");
+	System.out.println ("Hapy neu eare");
+	Scanner scanner = new Scanner (System.in);
+	int a = scanner.nextInt ();
+	System.out.println (a*a);
 	}
 }
